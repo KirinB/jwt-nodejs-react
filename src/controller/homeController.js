563ab0efc9d7271eb569c4c35410960907a4170c
kirinb <lee.minhnhan29@gmail.com>
2024-09-10 16:51:22 +0700

    use mysql2 POST form into mySQL

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,12 @@
+import mysql from "mysql2";
+
+//create the conection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "jwt",
+});
+
 const handleHelloWord = (req, res) => {
   return res.render("home.ejs");
 };
@@ -7,7 +16,26 @@ const handleUserPage = (req, res) => {
   return res.render("user.ejs");
 };
 
+const handleCreateNewUser = (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let username = req.body.username;
+
+  connection.query(
+    "INSERT INTO users (email, password, username) VALUES (?,?,?)",
+    [email, password, username],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+
+  return res.send("handleCreateNewUser");
+};
+
 module.exports = {
   handleHelloWord,
   handleUserPage,
+  handleCreateNewUser,
 };
